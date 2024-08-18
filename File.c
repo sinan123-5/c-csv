8bb965a6f2479b0e126c3dd4dff2c134e4e715b4
sinan123-5 <s.mertulucay@gmail.com>
2024-08-19 00:17:52 +0300

    No more Comment Hell
    Deleted unnecessary comments

--- File.c
+++ File.c
@@ -9,20 +9,20 @@ typedef enum {false, true} bool;
 // CSV File Structure
 typedef struct csv {
 	// File name and related data
-	short unsigned int nameLen; // File name's length
-	char* csvFile; // File's name
-	bool isPrepared; // Shows whether prepareCsv() executed succesfully or not
+	short unsigned int nameLen;
+	char* csvFile;
+	bool isPrepared;
 
 	// Data about lines in CSV file
-	unsigned int lineNum; // Number of lines in the file
-	unsigned int headerLine; // Line which contains the headers
-	unsigned long long size; // File size, the amount of characters it has
-	char seperator; // Seperator char, used to seperate one header's data from others
-	bool readyToRead; // Shows whether preReading() executed succesfully or not
+	unsigned int lineNum;
+	unsigned int headerLine;
+	unsigned long long size;
+	char seperator;
+	bool readyToRead;
 
 	// Headers
-	short unsigned int headerNum; // Number of headers
-	char* headers; // Header data in the format of "h1 sep h2 sep ... sep hn"
+	short unsigned int headerNum;
+	char* headers;
 } csv;
 
 // Function prototypes
@@ -50,7 +50,7 @@ int getHeaders(csv*file); // Extracts header data
 int getLines(csv*file, unsigned int startLine, unsigned int endLine, void*saveto); // Extracts data between lines (inclusive)
 
 int main(){
-	csv myCsv; // Sample Structure
+	csv myCsv;
 	prepareCsv(&myCsv, "myData.csv", 10); // Sample csv file
 
 	// Test log for preReading
@@ -58,26 +58,34 @@ int main(){
 	char str[200];
 	
 	getHeaders(&myCsv);
+	getLine(&myCsv, 1, str);
+	printf("%s\n", str);
+	
+	int i;
+	for(i = 0; i < 200; ++i){
+		str[i] = '\0';  
+	}
 	getLines(&myCsv, 2, 3 , str);
+	printf("%s\n", str);
 	printf("%s\n%lu\n", myCsv.headers, getLineLen(&myCsv, 2));
 }
 
 // Function to get a line's length
 unsigned long long getLineLen(csv*file, unsigned int lineNum){
-	if(lineNum > file->lineNum || !lineNum)return 1; // Exit with error code 1
+	if(lineNum > file->lineNum || !lineNum)return 1;
 
 	FILE*data;
-	data = fopen(file->csvFile, "r"); // Open Registered File
+	data = fopen(file->csvFile, "r");
 
-	unsigned int currentLine = 1; // Variable to count lines
-	char read; // Variable to store chars read from the file
+	unsigned int currentLine = 1;
+	char read;
 
 	// Loop until program gets to the start of the intended line (lineNum)
 	while(currentLine < lineNum){
-		read = getc(data); // 1 char read from the file
+		read = getc(data);
 
-		if(read ==10 || read == 13){
-			++currentLine; // If the char is a new line or a carriage return, increment the currentLine
+		if(read == '\r' || read == '\n'){
+			++currentLine;
 		}
 	}
 
@@ -86,7 +94,7 @@ unsigned long long getLineLen(csv*file, unsigned int lineNum){
 	 
 	
 	while(read != 10 && read != 13 && read != 0 && !feof(data)){
-		read = getc(data); // Continue reading from the file char by char
+		read = getc(data);
 	}
 	unsigned long lineEnd = ftell(data);
 	
@@ -96,12 +104,12 @@ unsigned long long getLineLen(csv*file, unsigned int lineNum){
 
 // Function to write the csv file's name into the pointed csv struct
 int prepareCsv(csv* file, const char* name, const short unsigned int nameLen){
-	file->nameLen = nameLen; // Register the file name's length to the struct, so as to alloc enough memory
-	file->csvFile = calloc(nameLen, 1); // Allocate memory for the file name
+	file->nameLen = nameLen
+	file->csvFile = calloc(nameLen, 1);
 
-	short unsigned int i = 0; // Create an i value to be used in the for loop
+	short unsigned int i = 0;
 
-	strcpy(file->csvFile, name); // Copy the name (parameter) into the file name in the csv struct
+	strcpy(file->csvFile, name);
 
 	// Loop to null out any characters that may have occured at the end of the file name
 	for(i = nameLen; i < sizeof(file); ++(i)){
@@ -111,49 +119,49 @@ int prepareCsv(csv* file, const char* name, const short unsigned int nameLen){
 	// If the file name or name length registered in the csv struct is different, exit with an error code
 	if(strcmp(file->csvFile, name) || !(file->nameLen == nameLen))return 1;
 
-	file->isPrepared = true; // Register to the struct that the file is prepared
-	return 0; // Return without error
+	file->isPrepared = true;
+	return 0;
 }
 
 // Function to calculate the file's size
 unsigned long long calculateSize(csv*file){
-	unsigned short int start = 0; // Points the start of the file
-	unsigned long long end = 0; // Points the end of the file
-	unsigned long long size = 0; // Stores the size of the file
+	unsigned short int start = 0;
+	unsigned long long end = 0;
+	unsigned long long size = 0;
 
 	FILE*data;
 
 	data = fopen(file->csvFile, "r");
 
-	fseek(data, 0, SEEK_SET); // Set file position indicator to the start of the file
-	start = ftell(data); // Save file's start pos
+	fseek(data, 0, SEEK_SET);
+	start = ftell(data);
 
-	fseek(data, 0, SEEK_END); // Set file position indicator to the end of the file
-	end = ftell(data); // Save file's end pos
+	fseek(data, 0, SEEK_END);
+	end = ftell(data);
 
-	size = end - start; // Substrack the value of start from the value of end to get the size
+	size = end - start;
 
-	size -= countLines(file); // New lines count as two bytes, 1 byte is substracted for each one to get the true size
+	size -= countLines(file);
 	
 	fclose(data);
-	return size; // Return the file's size
+	return size;
 }
 
 // Function to count the number of lines in file
 unsigned int countLines(csv*file){
-	 FILE*data; // File pointer
+	 FILE*data;
 
-	 data = fopen(file->csvFile, "r"); // Open registered file
-	 char read; // File reader to count new line characters, allocated 1 byte
-	 unsigned int lines = 0; // Variable to increment when a new line is read
+	 data = fopen(file->csvFile, "r");
+	 char read;
+	 unsigned int lines = 0;
 
 	 // Loop while end is not reached
 	 while(!feof(data)){
-		 read = getc(data); // Read a character from data
-		 if(read == 10 || read == 13) ++lines; // Increment the value of lines if character is a line feed or carriage return
+		 read = getc(data);
+		 if(read == 10 || read == 13) ++lines;
 	 }
-	 fclose(data); // Close the open file
-	 return lines; // Return the amount of counted lines
+	 fclose(data);
+	 return lines;
 }
 
 // Function to get csv file's length, seperator character and line of headers
@@ -162,7 +170,7 @@ int preReading(csv* file, const char sep, unsigned int headline){
 	if(!file->isPrepared) return 1; // Exit with error code 1
 
 	FILE *data;
-	data = fopen(file->csvFile, "r"); // Open the file registered in the csv struct
+	data = fopen(file->csvFile, "r");
 
 	//Check if the file exists
 	if(data == NULL) return 2; // Exit with error code 2 if the file doesn't
@@ -171,7 +179,7 @@ int preReading(csv* file, const char sep, unsigned int headline){
 	file->lineNum = countLines(file) + 1;
 
 	// Setting file->size
-	file->size = 	calculateSize(file); // Register the file's size into the csv struct
+	file->size = 	calculateSize(file);
 
 	// Seperator validation
 	if(!ispunct(sep))return 3; // If seperator is invalid, exits with error code 3
@@ -189,128 +197,81 @@ int getLine(csv*file, unsigned int lineNum, void*saveto){
 	if(lineNum > file->lineNum || !lineNum)return 1; // Exit with error code 1
 
 	FILE*data;
-	data = fopen(file->csvFile, "r"); // Open Registered File
+	data = fopen(file->csvFile, "r");
 
-	unsigned int currentLine = 1; // Variable to count lines
-	char read; // Variable to store chars read from the file
+	unsigned int currentLine = 1;
+	char read;
 
 	// Loop until program gets to the start of the intended line (lineNum)
 	while(currentLine < lineNum){
-		read = getc(data); // 1 char read from the file
+		read = getc(data);
 
 		if(read ==10 || read == 13){
-			++currentLine; // If the char is a new line or a carriage return, increment the currentLine
+			++currentLine;
 		}
 	}
 
-	read = getc(data); // Read the first char of the reached line (lineNum-th line)
-	unsigned int lineLen = 0; // Variable to store this line's length
-	
-	while(read != 10 && read != 13 && read != 0 && !feof(data)){
-		read = getc(data); // Continue reading from the file char by char
-		++lineLen; // Increment lineLen for every character read
-	}
-	
-	
-	fseek(data, -1 * (lineLen + 2), SEEK_CUR); // Set the file position indicator at the start of the line
+	read = getc(data);
+	unsigned int lineLen = getLineLen(file, lineNum);
 
-	char outputString[lineLen + 1]; // Create a String with the length of lineLen
-	unsigned int index = 0; // Variable storing which index of outputString to write in
-	unsigned int counter; // Variable for the loop below
+	char outputString[lineLen + 1];
+	unsigned int index = 0;
+	unsigned int counter;
 	for(counter = 0; counter < lineLen; counter++){
 		read = getc(data);
 		if(read == 10 ||read == 13){
 			--counter;
 			continue;
 		}
-		outputString[index] = read; // Write chars read from the file into the string
-		++index; // Select next index for writing
+		outputString[index] = read;
+		++index;
 	}
 	fclose(data);
-	outputString[lineLen] = '\0'; // Null character at the end to tell the program the string is finished
+	outputString[lineLen] = '\0';
 
-	memcpy(saveto, outputString, sizeof(outputString)); // Copy the outputString into intended destination
+	memcpy(saveto, outputString, sizeof(outputString));
 	return 0;
 }
 
 // Function to extract header data
 int getHeaders(csv*file){	
-	FILE*data;
-	data = fopen(file->csvFile, "r"); // Open Registered File
-
-	unsigned int currentLine = 1; // Variable to count lines
-	char read; // Variable to store chars read from the file
-
-	// Loop until program gets to the start of the intended line (lineNum)
-	while(currentLine < file->headerLine){
-		read = getc(data); // 1 char read from the file
-
-		if(read ==10 || read == 13){
-			++currentLine; // If the char is a new line or a carriage return, increment the currentLine
-		}
-	}
-
-	read = getc(data); // Read the first char of the reached line (file->headerLine-th line)
-	unsigned int lineLen = 0; // Variable to store this line's length
-	
-	while(read != 10 && read != 13 && read != 0 && !feof(data)){
-		read = getc(data); // Continue reading from the file char by char
-		++lineLen; // Increment lineLen for every character read
-	}
-	
-	file->headers = calloc(lineLen, 1); // Allocate lineLen x 1 bytes of space to the headers string
-	
-	fseek(data, -1 * (lineLen + 2), SEEK_CUR); // Set the file position indicator at the start of the line
-
-	char outputString[lineLen + 1]; // Create a String with the length of lineLen
-	unsigned int index = 0; // Variable storing which index of outputString to write in
-	unsigned int counter; // Variable for the loop below
-	for(counter = 0; counter < lineLen; counter++){
-		read = getc(data);
-		if(read == 10 ||read == 13){
-			--counter;
-			continue;
-		}
-		outputString[index] = read; // Write chars read from the file into the string
-		++index; // Select next index for writing
-	}
-	fclose(data);
-	outputString[lineLen] = '\0'; // Null character at the end to tell the program the string is finished	
-	strcpy(file->headers, outputString);
-	return 0;
+	unsigned int headerLen = getLineLen(file, file->headerLine);
+	char headerString[headerLen + 1];
+	headerString[headerLen] = '\0';
+	return getLine(file, file->headerLine, headerString);
 }
 
 // Function to extract data between lines (inclusive)
 int getLines(csv*file, unsigned int startLine, unsigned int endLine, void*saveto){
-	unsigned long long totalSize = 0; // Total size of the data between the lines
-	unsigned int index = 0; // Variable for loops below
-	unsigned long maxLineLen = 0; // Highest line length
-	unsigned long currentLen; // Lenght of the current line
+	unsigned long long totalSize = 0;
+	unsigned int index = 0;
+	unsigned long maxLineLen = 0;
+	unsigned long currentLen;
 	
 	// Loop for calculating the max line length and the total size
 	for(index = startLine; index <= endLine; ++index){
-		currentLen = getLineLen(file, index); // currentLen is set as the index-th line's length
-		totalSize += currentLen; // Add currentLen to totalSize
-		if(maxLineLen < currentLen) maxLineLen = currentLen; // If the length of this line is greater than maxLineLen, maxLineLen is current line's length
+		currentLen = getLineLen(file, index);
+		totalSize += currentLen;
+		if(maxLineLen < currentLen) maxLineLen = currentLen;
 	}
 	
-	char outputString[totalSize + 1]; // String to be saved
-	outputString[totalSize] = '\0'; // Last char of outputString is NULL, denoting the string is finished
-	char buffer[maxLineLen]; // A buffer that gets the content of a single line at a time
-	unsigned int bufferCleaner; // Loop variable for emptying buffer
+	char outputString[totalSize + 1];
+	outputString[totalSize] = '\0';
+	char buffer[maxLineLen];
+	unsigned int bufferCleaner;
 	
 	// Loop until the last line is read
 	for(index = startLine; index <= endLine; ++index){
-		getLine(file, index, buffer); // Save the index-th line in buffer
-		strcat(outputString, buffer); // Append buffer to outputString
-		strcat(outputString, "\r\n"); // Append a carriage return and a line feed to outputString
+		getLine(file, index, buffer);
+		strcat(outputString, buffer);
+		strcat(outputString, "\r\n");
 		
 		// Clean the buffer
 		for(bufferCleaner = 0; buffer[bufferCleaner] != 0; ++bufferCleaner)buffer[bufferCleaner] = '\0';
 	}
 	
 	printf("%s\n", outputString); // (Debugging) Print the outputString
-	memcpy(saveto, outputString, sizeof(outputString)); // Copy the outputString to the intended memory adress
+	memcpy(saveto, outputString, sizeof(outputString));
 
 	return 0;
 }
