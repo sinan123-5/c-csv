fb221422dc17f4e5e3badf094377e5f3cec7c3f1
sinan123-5 <s.mertulucay@gmail.com>
2024-08-20 22:23:04 +0300

    Sources.c content added
    Cut from Main Structures.c(deleted)

---
+++ Sources/Sources.c
@@ -0,0 +1,21 @@
+// Sources for testing
+typedef struct csv {
+	// File name and related data
+	short unsigned int nameLen;
+	char* csvFile;
+	bool isPrepared;
+
+	// Data about lines in CSV file
+	unsigned int lineNum;
+	unsigned int headerLine;
+	unsigned long long size;
+	char seperator;
+	bool readyToRead;
+
+	// Headers
+	short unsigned int headerNum;
+	char* headers;
+} csv;
+
+// Boolean enum
+typedef enum {false, true} bool;
